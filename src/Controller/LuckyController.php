26056414526fc11f3637d067ba98f78f16ccc7b7
Pilathraj <pilathraj@gmail.com>
2017-12-12 18:46:01 +0530

    Annotation has implemented

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -1,10 +1,20 @@
-<?php 
+<?php
 namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
 
 class LuckyController{
   public function number(){
     return new Response('Hello pilathraj..');
+  }
+  
+  /**
+    * @Route("/lucky/number2")
+    */
+  public function number2(){
+    return new Response('from Cse..');
   }
 }
 
